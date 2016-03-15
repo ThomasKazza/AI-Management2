2633b2d31777f74507904982ebdc1138ac51ee37
ThomasKazza <thomaskazza@gmail.com>
2016-03-15 09:58:18 +0100

    Added files via upload

---
+++ HellowOrld.java
@@ -0,0 +1,31 @@
+package helloworld1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class HellowOrld extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)
+	{
+		try
+		{
+			response.setContentType("text/html");
+			PrintWriter printWriter = response.getWriter();
+			printWriter.println("<h2>");
+			printWriter.println("Hello World");
+			printWriter.println("</h2>");
+		}
+		catch (IOException ioException)
+		{
+			ioException.printStackTrace();
+		}
+	}
+
+}
