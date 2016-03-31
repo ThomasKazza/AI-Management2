0a2124aefd0ac4ee970c706e75519d6f09193a93
ThomasKazza <thomaskazza@gmail.com>
2016-03-31 11:32:17 +0200

    Added files via upload

---
+++ User.java
@@ -0,0 +1,25 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+	private String password;
+	private String username;
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
+
+
